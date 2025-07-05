47b3922487d4a73e5eec38b82d281cdd81c69f93
Jiaxin.Zhou <77322342+zhou277@users.noreply.github.com>
2025-07-05 17:07:10 +0100

    Add files via upload

--- QCL_structure.py
+++ QCL_structure.py
@@ -1,25 +1,25 @@
 import meep as mp
 import numpy as np
 
-# 各层厚度（单位：μm）
-resolution = 50
-sx = 7000
-sy = 350
-sz = 120
-cell = mp.Vector3(0, sy, sz)
-pml_layers = [mp.PML(0.1)]
+# basic setup (0.1 μm)
+resolution = 100
+sx = 70
+sy = 35
+sz = 12
+cell = mp.Vector3(sx, sy, 0)
+pml_layers = [mp.PML(1)]
 
-# 各层厚度（单位：μm）
-au_thickness = 0.18
-ti_thickness = 0.02
-pdge_thickness = 0.1
-ngaas_thickness_1 = 0.08
-Ar_thickness = 0.0113
+# thickness of different layers（0.1 μm）
+au_thickness = 0.018
+ti_thickness = 0.002
+pdge_thickness = 0.01
+ngaas_thickness_1 = 0.008
+Ar_thickness = 0.00113
 
-ngaas_thickness_2 = 0.7
-sigaas_thickness = 300
+ngaas_thickness_2 = 0.07
+sigaas_thickness = 30
 
-# 材料定义（近似 ε = n^2）
+# material definition
 au = mp.Medium(epsilon=9.5)
 ti = mp.Medium(epsilon=6.0)
 pdge = mp.Medium(epsilon=10.0)
@@ -32,40 +32,71 @@ geometry = []
 # Substrate
 geometry.append(mp.Block(
                     material=si_gaas,
-                    size=mp.Vector3(mp.inf, sigaas_thickness,sz),
+                    size=mp.Vector3(sx, sigaas_thickness,sz),
                     center=mp.Vector3(y= -sy/2 + sigaas_thickness/2)))
 
 geometry.append(mp.Block(
                     material=ngaas,
-                    size=mp.Vector3(mp.inf, ngaas_thickness_2,sz),
-                    center=mp.Vector3(y= -sy/2 + sigaas_thickness/2 + ngaas_thickness_2/2)))
+                    size=mp.Vector3(sx, ngaas_thickness_2,sz),
+                    center=mp.Vector3(y= -sy/2 + sigaas_thickness + ngaas_thickness_2/2)))
 
 # QCL active region
 geometry.append(mp.Block(
                     material=Ar,
-                    size=mp.Vector3(mp.inf, Ar_thickness,sz),
-                    center=mp.Vector3(y= -sy/2 + sigaas_thickness/2 + ngaas_thickness_2/2 + Ar_thickness/2)))
+                    size=mp.Vector3(sx, Ar_thickness,sz),
+                    center=mp.Vector3(y= -sy/2 + sigaas_thickness + ngaas_thickness_2 + Ar_thickness/2)))
 
-# 上层金属叠层
-y0 = -sy/2 + sigaas_thickness/2 + ngaas_thickness_2/2 + Ar_thickness/2
+# upper layers
+y0 = -sy/2 + sigaas_thickness + ngaas_thickness_2 + Ar_thickness
 geometry += [
     mp.Block(material = Ar,  
-             size = mp.Vector3(mp.inf, Ar_thickness,sz), 
-             center = mp.Vector3(y=y0 + Ar_thickness/2)),
+             size = mp.Vector3(sx, Ar_thickness,sz), 
+             center = mp.Vector3(y = y0 + Ar_thickness/2)),
     mp.Block(material = ngaas, 
-             size = mp.Vector3(mp.inf, ngaas_thickness_1,sz), 
-             center = mp.Vector3(y=y0 + Ar_thickness + ngaas_thickness_1/2)),
+             size = mp.Vector3(sx, ngaas_thickness_1,sz), 
+             center = mp.Vector3(y = y0 + Ar_thickness + ngaas_thickness_1/2)),
     mp.Block(material = pdge,    
-             size = mp.Vector3(mp.inf, pdge_thickness,sz),    
-             center = mp.Vector3(y=y0 + Ar_thickness + ngaas_thickness_1/2 + pdge_thickness/2)),
+             size = mp.Vector3(sx, pdge_thickness,sz),    
+             center = mp.Vector3(y = y0 + Ar_thickness + ngaas_thickness_1 + pdge_thickness/2)),
     mp.Block(material = ti,    
-             size = mp.Vector3(mp.inf, ti_thickness,sz),    
-             center = mp.Vector3(y=y0 + Ar_thickness + ngaas_thickness_1/2 + pdge_thickness/2 + ti_thickness/2)),
+             size = mp.Vector3(sx, ti_thickness,sz),    
+             center = mp.Vector3(y = y0 + Ar_thickness + ngaas_thickness_1 + pdge_thickness + ti_thickness/2)),
     mp.Block(material = au,    
-             size = mp.Vector3(mp.inf, au_thickness,sz),    
-             center = mp.Vector3(y=y0 + Ar_thickness + ngaas_thickness_1/2 + pdge_thickness/2 + ti_thickness/2 + au_thickness/2))
+             size = mp.Vector3(sx, au_thickness,sz),    
+             center = mp.Vector3(y = y0 + Ar_thickness + ngaas_thickness_1 + pdge_thickness + ti_thickness + au_thickness/2))
 ]
     
+# grating parameters
+Λ = 0.5                    # 基本周期（0.1μm）
+slot_length = 10
+slot_width = 0.1        
+slot_depth = 0.5       
+slot_spacing = Λ / 2
+pattern_1 = "|-||-|-||||||-|||||-||||-|||-||-||||"  # 狭缝排列样式
+pattern_2 = "-|||||||-||-||-|||-||-|-||-|||-||-|-||||||-||-|-|||-||-|||||-|||"
+pattern_3 = "-||-||-||-|||||-||||||-|||-||||-||-||-|||||||||-||-|-||-||-||-|-|||-||-||-|-||||||-||||||||-||-||||||-||||---|"
+pattern = pattern_1 + pattern_2 + pattern_3
+
+
+x_start = -len(pattern) * slot_spacing / 2
+y_grating = y0 + Ar_thickness + ngaas_thickness_1 + pdge_thickness + ti_thickness + au_thickness/2
+
+# grating list
+grating_geometry = []
+
+# setup grating list
+for i, symbol in enumerate(pattern):
+    if symbol == "|":
+        x_pos = x_start + i * slot_spacing
+        grating_geometry.append(mp.Block(
+                                material=mp.air,
+                                size=mp.Vector3(slot_width, slot_depth, sz),
+                                center=mp.Vector3(x_pos,y_grating,0)
+        ))
+
+# add to original layers
+geometry += grating_geometry
+
 # Source
 sources = [mp.Source(
     src=mp.GaussianSource(frequency=1/0.1, fwidth=0.2),
@@ -83,6 +114,4 @@ sim = mp.Simulation(
     dimensions=2
 )
 
-
-sim.plot2D(output_plane=mp.Volume(center=mp.Vector3(0,0,0), 
-                                  size=mp.Vector3(0, y=sim.cell_size.y, z=sim.cell_size.z)))
+sim.plot2D()
