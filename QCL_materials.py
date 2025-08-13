2ec10502f3847f94cc2e3126d3ce26adad5addb9
Jiaxin.Zhou.linux3 <1604226088@qq.com>
2025-08-13 20:58:54 +0100

    8.13

--- QCL_materials.py
+++ QCL_materials.py
@@ -1,39 +1,6 @@
 import meep as mp
 import math
 
-light_speed = 3e8
-meep_trans = 1e-6
-
-Au = mp.Medium(
-    epsilon=1.0,
-    E_susceptibilities=[
-        mp.DrudeSusceptibility(frequency=0.44, gamma=0.033, sigma=1.0)])
-
-# Material in waveguide experiments
-    ## GaAs N = 5e18 cm-3 used in the middle layer between Au and Active Region (n-Si doping)
-nSI_GaAs = mp.Medium(
-    epsilon=10.9,
-    E_susceptibilities=[
-        mp.DrudeSusceptibility(frequency=1.62, 
-                               gamma=0.033, 
-                               sigma=1.0)])
-    ## GaAs N = 0 or 5e15 cm-3 used in Active Region
-GaAs_active = mp.Medium(
-    epsilon=12.25,
-    E_susceptibilities=[
-        mp.DrudeSusceptibility(frequency=0.0513, 
-                               gamma=0.0033, 
-                               sigma=1.0)])
-    ## substrate GaAs N = 1~2e18 cm-3 used in substrate 
-substrate_GaAs = mp.Medium(
-    epsilon=10.9,
-    E_susceptibilities=[
-        mp.DrudeSusceptibility(frequency=0.7252,   
-                               gamma=0.0333,       
-                               sigma=1.0)])
-
-
-###--------------Another way of setting up the material by Drude--------------###
 """refer to the official document,using the meep unit"""
 def drude_medium(N_cm3, tau_ps, m_eff_ratio, eps_inf)-> mp.Medium:
     # Constants
@@ -46,11 +13,11 @@ def drude_medium(N_cm3, tau_ps, m_eff_ratio, eps_inf)-> mp.Medium:
     unit_freq = c / a
 
     # Convert units
-    N = N_cm3 * 1e6  # from cm^-3 to m^-3
+    N_m3 = N_cm3 * 1e6  # from cm^-3 to m^-3
     tau = tau_ps * 1e-12  # from ps to s
 
     # Plasma frequency: ω_p^2 = Ne^2 / (eps0 * m*)
-    omega_p2_SI = N * e ** 2 / (eps0 * m_eff)  # in rad^2/s^2
+    omega_p2_SI = N_m3 * e ** 2 / (eps0 * m_eff)  # in rad^2/s^2
     gamma_SI = 1 / tau  # in Hz
 
     # Convert to Meep units (divide by unit_freq)
@@ -58,21 +25,40 @@ def drude_medium(N_cm3, tau_ps, m_eff_ratio, eps_inf)-> mp.Medium:
     gamma_meep = gamma_SI / unit_freq
 
     # Return Meep medium object
-    return mp.Medium(
-        epsilon=eps_inf,
-        E_susceptibilities=[
-            mp.DrudeSusceptibility(frequency=0, gamma=gamma_meep, sigma=omega_p2_meep)
-        ])
+    return mp.Medium(epsilon=eps_inf,
+                    E_susceptibilities=[mp.DrudeSusceptibility(frequency=0, 
+                                                                gamma=gamma_meep, 
+                                                                sigma=omega_p2_meep
+                                   )])
 
-material_au = drude_medium(N_cm3=5.6e22, tau_ps=0.05, m_eff_ratio=1.0, eps_inf=1.0)
-material_gaas_doped = drude_medium(N_cm3=5e18, tau_ps=0.1, m_eff_ratio=0.067, eps_inf=12.9)
-material_gaas_lightly_doped = drude_medium(N_cm3=5e15, tau_ps=1.0, m_eff_ratio=0.067, eps_inf=12.9)
-material_gaas_substrate = drude_medium(N_cm3=1e18, tau_ps=0.1, m_eff_ratio=0.067, eps_inf=12.9)
+Au_fun         = drude_medium(N_cm3=5.6e22, tau_ps=0.05, m_eff_ratio=1.0,   eps_inf=1.0 )
+GaAs_5e18_fun  = drude_medium(N_cm3=5.0e18, tau_ps=0.1,  m_eff_ratio=0.067, eps_inf=12.9)
+GaAs_5e15_fun  = drude_medium(N_cm3=5.0e15, tau_ps=1.0,  m_eff_ratio=0.067, eps_inf=12.9)
+GaAs_1e18_fun  = drude_medium(N_cm3=1.0e18, tau_ps=0.1,  m_eff_ratio=0.067, eps_inf=12.9)
+GaAs_0_fun  = drude_medium(N_cm3=0, tau_ps=1.0,  m_eff_ratio=0.067, eps_inf=12.9)
 
-###--------------Do not use the Drude model to identify the material--------------###
+
+"""Using the approximated parameter model"""
 # Au
 Au_XD = mp.Medium(index=2.21)
 ## GaAs N = 5e18 cm-3 used in the middle layer between Au and Active Region (n-Si doping)
 GaAs_High_XD = mp.Medium(index=4.55)
 ## GaAs N = 0 or 5e15 cm-3 used in Active Region
 GaAs_Act_XD = mp.Medium(index=3.45)
+
+"""Using the complex conductivity"""
+def transfer_to_meep_frequency(frequency:int)->int:
+    um = 1e-6
+    c = 3e8 
+    frequency_in_meep = frequency * um / c
+    return frequency_in_meep
+
+# Electronically tunable aperiodic distributed feedback terahertz lasers
+# material in the above artical
+# the details are in the Fig 1 part c and d
+# c is a standard SI-SP waveguide at 2.85THz and d is same structure after the removal of the metal overlayers
+# This material is used to plot the mode intensity
+freq_in_meep = transfer_to_meep_frequency(2.85e12)
+
+QCL_neff_c = mp.Medium(epsilon=3.647, D_conductivity=2*math.pi*freq_in_meep*0.007/3.647)
+QCL_neff_d = mp.Medium(epsilon=3.308, D_conductivity=2*math.pi*freq_in_meep*0.217/3.308)
